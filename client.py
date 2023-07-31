c411587897df48cf246aa62284b2b96a7b39cda8
Kartman570 <kotelnikovilya@yandex.ru>
2023-08-01 03:07:36 +0400

    update

--- client.py
+++ client.py
@@ -6,59 +6,71 @@ import sys
 HOST = "127.0.0.1"
 PORT = 12345
 KEEP_ALIVE_DELAY = 1
-IS_RUNNING = True
 
+is_running = True
 my_nickname = ""
 input_text = ""
+
 
 def send_message(s: socket.socket) -> None:
     global input_text
     while True:
         input_text = input(f"{my_nickname}(you) - ")
-        s.sendall((my_nickname + ":" + input_text).encode())
+        message = my_nickname + input_text
+        message = str(len(my_nickname)) + ":" + message
+        s.sendall(message.encode())
         if input_text == "quit:":
-            IS_RUNNING = False
+            is_running = False
             break
+
 
 def receive_message(s: socket.socket) -> None:
     global input_text
-    global IS_RUNNING
-    while IS_RUNNING:
+    global is_running
+    while is_running:
         data = s.recv(1024)
         if not data:
             break
-        message = data.decode()
-        nickname, message = message.split(":", 1)
+        recieved = data.decode()
+        delimiter, message = recieved.split(":", 1)
+        nickname = message[:int(delimiter)]
+        message = message[int(delimiter):]
         if nickname == "server" and message == "SHUTDOWN":
             print("\nSERVER SHUTTED DOWN")
-            IS_RUNNING = False
+            is_running = False
             break
         print("\r" + " " * (len(input_text) + len(my_nickname) + 10), end="")
         print(f"\r{nickname} - {message}")
         print(f"{my_nickname}(you) - {input_text}", end="")
         sys.stdout.flush()
-        
+
 
 def keep_alive():
-    global IS_RUNNING
-    while IS_RUNNING:
-        s.sendall((my_nickname + ":keepalive:").encode())
+    global is_running
+    while is_running:
+        message = my_nickname + "keepalive:"
+        message = str(len(my_nickname)) + ":" + message
+        s.sendall(message.encode())
         time.sleep(KEEP_ALIVE_DELAY)
 
 
 while True:
     my_nickname = input("Enter your nickname - ")
-    if ":" in my_nickname:
-        print("char ':' not alowed for username")
-    elif my_nickname == "server":
-        print("'server' is reserved system name")
+    if my_nickname == "server" or my_nickname == "system":
+        print("'server' and 'system' are reserved system names")
     else:
         break
 
-print(f"\nHello, {my_nickname}. Connecting to server...\nFor exit type 'quit:'\nTo see online list, type 'online:'\n")
+
+print(f"""Hello, {my_nickname}. Connecting to server...
+For exit type 'quit:'
+To see online list, type 'online:'""")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(str(my_nickname + ":new_user:").encode())
+
+message = "system" + my_nickname
+message = str(len("system")) + ":" + message
+s.sendall(message.encode())
 
 send_thread = threading.Thread(target=send_message, args=(s,), daemon=True)
 keep_alive_thread = threading.Thread(target=keep_alive, daemon=True)
@@ -70,4 +82,3 @@ keep_alive_thread.start()
 
 recv_thread.join()
 s.close()
-
--- server.py
+++ server.py
@@ -5,101 +5,148 @@ import time
 
 HOST = "127.0.0.1"
 PORT = 12345
-IS_RUNNING = True
 KEEP_ALIVE_SEC = 5
+
+is_running = True
 clients = {}
 logs = []
 
 
-def send_everyone(msg: str) -> None:
+def send_everyone(user_from: str, text: str) -> None:
     for nick, client_info in clients.items():
-        client_info["connection"].sendall(msg.encode())
+        message = user_from + text
+        message = str(len(user_from)) + ":" + message
+        client_info["connection"].sendall(message.encode())
 
 
-def send_everyone_except(msg: str, except_client: str) -> None:
+def send_everyone_except(user_from: str, text: str, user_except: str) -> None:
     for nick, client_info in clients.items():
-        if nick != except_client:
-            client_info["connection"].sendall(msg.encode())
+        if nick != user_except:
+            message = user_from + text
+            message = str(len(user_from)) + ":" + message
+            client_info["connection"].sendall(message.encode())
 
 
-def send_to(msg: str, target_client: str) -> None:
-    clients[target_client]["connection"].sendall(msg.encode())
+def send_to(user_from: str, text: str, user_target: str) -> None:
+    message = user_from + text
+    message = str(len(user_from)) + ":" + message
+    clients[user_target]["connection"].sendall(message.encode())
 
 
 def new_client(connection: socket.socket, addr) -> None:
     nickname = None
-    client_info = {"address": addr, "connection": connection, "last_keepalive": time.time(), "is_active": True}
+    client_info = {"address": addr,
+                   "connection": connection,
+                   "last_keepalive": time.time(),
+                   "is_active": True}
     try:
-        while IS_RUNNING and client_info["is_active"]:
+        while is_running and client_info["is_active"]:
             data = connection.recv(1024)
             if not data:
                 break
-            message = data.decode()
-            nickname, message = message.split(":", 1)
-            
-            
+
+            recieved = data.decode()
+            delimiter, message = recieved.split(":", 1)
+            nickname = message[:int(delimiter)]
+            message = message[int(delimiter):]
+
             if message == "keepalive:":
                 clients[nickname]['last_keepalive'] = time.time()
                 continue
-            
-            logs.append(";".join(( \
-             time.ctime(time.time()), \
-             str(addr), \
-             nickname, \
-             message \
-             )))
-            if message == "new_user:":
+
+            logs.append({
+             "time": time.strftime("%d.%m.%Y %H:%M:%S", time.localtime()),
+             "addr": str(addr),
+             "nickname": nickname,
+             "message": message
+             })
+            if nickname == "system":
+                nickname = message
                 clients[nickname] = client_info
-                send_everyone("server:" + nickname + " joined")
+                send_everyone("server", nickname + " joined")
                 print(f"{nickname} joined")
                 continue
             if message == "quit:":
                 break
             if message == "online:":
-                send_to("server:" + ", ".join(clients),nickname)
+                send_to("server", ", ".join(clients), nickname)
                 continue
 
             print(f"{nickname} - {message}")
-            send_everyone_except(nickname + ":" + message, nickname)
+            send_everyone_except(nickname, message, nickname)
     except socket.error:
         print(f"Lost connection to {nickname}")
     finally:
-        send_everyone("server:" + nickname + " left")
+        send_everyone("server", nickname + " left")
         print(f"{nickname} Left")
         if nickname in clients:
             del clients[nickname]
         connection.close()
 
-def console(s : socket.socket) -> None:
-    print('''Succesfuly started.
-     
+
+def console(s: socket.socket) -> None:
+    print("Succesfuly started.")
+    help_msg = ('''
      commands:
-     logs %username% - show all messages from %username%
-     online - show who is connected now
-     stop - send deauth to online users and shutdown server
+     logs: -u %username%   show all messages from %username%
+     logs: -t %time or date%   show all messages at time or date
+     logs: -a   show all messages
+     online:   show who is connected now
+     help:   shwo this message
+     stop:   send deauth to online users and shutdown server
      ''')
+    print(help_msg)
     while True:
         command = input()
-        if command == "stop":
+        if command == "help:":
+            print(help_msg)
+            continue
+        if command == "stop:":
+            while command != "Y" and command != "N":
+                command = input("SHUTDOWN SERVER\nARE YOU SURE? Y/N").upper()
+            if command == "N":
+                continue
             print("SHUTDOWN SERVER")
-            IS_RUNNING = False
             for nick, client_info in clients.items():
-                client_info["connection"].sendall("server:SHUTDOWN".encode())
+                send_everyone("server", "SHUTDOWN")
+            is_running = False
             break
-        if command == "online":
+        if command == "online:":
             for nick, client_info in clients.items():
                 print(nick + str(client_info["address"]))
             continue
-        if command.startswith("logs"):
-            _, _, username = command.partition(" ")
-            for event in logs:
-                if username in event:
-                    print(event)
+        if command.startswith("logs:"):
+            command_parts = command.split()
+            if len(command_parts) == 1:
+                print("""Missing argument. To see logs use arguments:
+                logs: -u %username%   show all messages from %username%
+                logs: -t %time or date%   show all messages at time or date
+                logs: -a   show all messages
+                """)
+            else:
+                argument = command_parts[1]
+                value = command_parts[2] if len(command_parts) > 2 else None
+                if value == "":
+                    print("Missing argument`s value")
+                if argument == "-u":
+                    for log in logs:
+                        if log['nickname'] == value:
+                            print(f"{log['time']};{log['addr']};{log['nickname']};{log['message']}")
+                elif argument == "-t":
+                    for log in logs:
+                        if value in log['time']:
+                            print(f"{log['time']};{log['addr']};{log['nickname']};{log['message']}")
+                elif argument == "-a":
+                    for log in logs:
+                        print(f"{log['time']};{log['addr']};{log['nickname']};{log['message']}")
+                else:
+                    print("Invalid command")
             continue
-        send_everyone("server:"+command)
+        send_everyone("server", command)
 
-def accept_thread(s : socket.socket) -> None:
-    while IS_RUNNING:
+
+def accept_thread(s: socket.socket) -> None:
+    while is_running:
         conn, addr = s.accept()
         threading.Thread(target=new_client, args=(conn, addr), daemon=True).start()
 
@@ -108,11 +155,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
-    
-    console_thread = threading.Thread(target=console, args=(s,), daemon=True)
-    console_thread.start()
-    
+
     threading.Thread(target=accept_thread, args=(s,), daemon=True).start()
-    
-    console_thread.join()
+    console(s)
     s.close()
